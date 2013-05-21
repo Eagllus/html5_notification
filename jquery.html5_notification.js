df61d8d47c1339659212f5114793c5db53767153
Ronald van Zon <ronald.vanZon@nedap.com>
2013-05-21 08:43:48 +0200

    Initial Commit

---
+++ jquery.html5_notification.js
@@ -0,0 +1,160 @@
+(function($) {
+	var html5Notification = {
+		init: function ( options ) {
+			// Allow to override default options.
+			this.buildDefaults();
+
+			// Check browser support
+			if (this.check_browser_support() ) {
+
+				// Supported, ask for permission
+				this.permissionHandler();
+			}
+
+			return this;
+
+		},
+
+		permissionHandler: function() {
+			// Check if we have permission (setting output to variable)
+			var checkPermission = this.check_permission();
+			
+			if ( checkPermission === true) {
+
+			} else if ( checkPermission === 'pending') {
+				this.request_permission();
+
+			} else {
+				this.config.field.browser_support
+					.addClass("alert alert-error")
+					.text(this.config.message.permission_denied);
+			}
+		},
+
+		// Default settings that can be changed by user.
+		buildDefaults: function() {
+			this.config = {
+				display_message: 			true,
+
+				message: {
+					supported_browser:	    'Your browser does not support the Notification API.',
+					notsupported_browser: 	'Your browser does support the Notification API.',
+					permission_denied: 	    'You have denied access to display notifications.',
+					permission_button: 		'Grant Permission To Display Notifications',
+				},
+
+				field: {
+					container: 				$('body'),
+					browser_support:		$('<div id="message" /></div>'),
+				}
+			};
+		},
+
+		/**
+		* Check if browser can support HTML5 Notifications
+		*/
+		check_browser_support: function() {
+			var supported = (Notification) ? true : false;
+			if (this.config.display_message === true) {
+				var browser_support = this.config.field.browser_support.appendTo(this.config.field.container);	
+
+				if( supported === true ) {
+					browser_support
+						.addClass("alert alert-success")
+						.text(this.config.message.notsupported_browser);
+				} else {
+					browser_support
+						.addClass("alert alert-error")
+						.text(this.config.message.supported_browser);
+				}
+			}
+
+			return supported;
+		},
+
+		/**
+		* Checks to see if HTML5 Notifications has permission
+		*/
+		check_permission: function() {
+			switch( Notification.permission ) {
+				// We have permission to post notifications
+				case 0:
+				case 'granted':
+					return true;
+					break;
+
+				// We still need to ask for permission
+				case 1:
+				case 'default':
+					return 'pending';
+					break;
+
+				// The user rejected the permissions to post notifications
+				case 2:
+				case 'denied':
+					return false;
+					break;
+			}
+		},
+
+		/**
+		* Request HTML5 Notifications permissions.
+		* After that recall permissionHandler
+		*/
+		request_permission: function () {
+			var self = this;
+
+			Notification.requestPermission(function(){
+				self.permissionHandler();
+			});
+		},
+
+		/**
+		* All the default options can be inserted into the Notification Api.
+		* For a complete list of options see the W3 Web Notifications url.
+		* https://dvcs.w3.org/hg/notifications/raw-file/tip/Overview.html#api
+		*
+		* If the permission is not already granted, the init function will be called.
+		* This will cause the current message to be losted.
+		*
+		* TODO: Fix prevent losing of the current message
+		*/
+		create_message: function( options ) {
+			if (Notification.permission == 'granted') {
+				if ( typeof options === 'string') {
+					var title = options,
+						config = {};
+				} else {
+					var title = options.title,
+						config = options;
+				}
+
+				new Notification(title, config);
+
+			} else {
+				this.init();
+				// this.create_message(options);
+			}
+		}
+	};
+
+	/**
+	* Create a plain text notification box
+	*/
+	function plain_text_notification(image, title, content) {
+		if (window.webkitNotifications.checkPermission() == 0) {
+			return window.webkitNotifications.createNotification(image, title, content);
+		}
+	}
+
+	/**
+	* Create a notification box with html inside
+	*/
+	function html_notification(url) {
+		if (window.webkitNotifications.checkPermission() == 0) {
+			return window.webkitNotifications.createHTMLNotification(url);
+		}
+	}
+
+	window.html5Notification = html5Notification;
+})( jQuery);
